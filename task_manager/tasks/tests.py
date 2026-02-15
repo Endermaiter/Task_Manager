a039f8c692cf9bfc72810f8d77970006b8864ed9
Endermaiter <marcospnovoa@gmail.com>
2026-02-15 14:56:09 +0100

    Create Test Cases to test API

--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Family, Task
+from datetime import date, timedelta
 
-# Create your tests here.
+class FamilyAndTaskTest(TestCase):
+
+    def create_family(self):
+        self.family = Family.objects.create(name="Family_Test")
+        self.assertEqual(self.family.name, "Family_Test")
+
+    def create_task(self):
+        task = Task.objects.create(
+            title="Task_Test",
+            description="Description_Test",
+            state="to do",
+            expiration_date=date.today() + timedelta(days=7),
+            family=self.family
+        )
+
+        self.assertEqual(task.title, "Task_Test")
+        self.assertEqual(task.family, self.family)
+        self.assertEqual(task.state, "to do")
