fbcb9dd46a6f49834d0a255cf366a39baeb6b619
Endermaiter <marcospnovoa@gmail.com>
2026-02-16 20:58:10 +0100

    Remove Policy check in ps1

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
-Write-Host "🚀 Starting setup..." -ForegroundColor Cyan
+﻿Write-Host "🚀 Starting setup..." -ForegroundColor Cyan
 
 $originalPath = Get-Location
 
@@ -6,19 +6,10 @@ $root = $PSScriptRoot
 $backendPath = Join-Path $root "task_manager_api"
 $frontendPath = Join-Path $root "task_manager_frontend"
 
-$currentPolicy = Get-ExecutionPolicy -Scope CurrentUser
-if ($currentPolicy -ne "RemoteSigned" -and $currentPolicy -ne "Unrestricted") {
-    Write-Host "⚠️ The execution policy does not allow running scripts. Please run this first:" -ForegroundColor Yellow
-    Write-Host "Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned" -ForegroundColor Cyan
-    exit 1
-}
-Write-Host "✅ Execution policy is correct. Continuing..." -ForegroundColor Green
-
 Write-Host "🐍 Installing backend dependencies..." -ForegroundColor Green
 poetry install --no-root --directory "$backendPath"
 Write-Host "✅ Backend dependencies installed." -ForegroundColor Green
 
-# Show installed packages
 poetry show --directory "$backendPath"
 
 Write-Host "🟢 Installing frontend dependencies..." -ForegroundColor Green
